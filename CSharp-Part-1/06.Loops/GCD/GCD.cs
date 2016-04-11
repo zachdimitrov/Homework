0d01d7a302eabfbe0b10d62700c0e0f7882523db
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2016-04-11 23:54:51 +0300

    Loops

--- CSharp-Part-1/06.Loops/GCD/GCD.cs
+++ CSharp-Part-1/06.Loops/GCD/GCD.cs
@@ -8,8 +8,23 @@ class GCD
 {
     static void Main()
     {
-        int A = int.Parse(Console.ReadLine());
-        int B = int.Parse(Console.ReadLine());
-
+        int A = Math.Abs(int.Parse(Console.ReadLine()));
+        int B = Math.Abs(int.Parse(Console.ReadLine()));
+        while (true)
+        {
+            if (A == B)
+            {
+                Console.WriteLine(A);
+                break;
+            }
+            if (A > B)
+            {
+                A = A - B;
+            }
+            if (B > A)
+            {
+                B = B - A;
+            }
+        }
     }
 }
--- CSharp-Part-1/06.Loops/Trailing 0 in N/Trailing0toN.cs
+++ CSharp-Part-1/06.Loops/Trailing 0 in N/Trailing0toN.cs
@@ -8,5 +8,18 @@ class Trailing0toN
 {
     static void Main()
     {
+        int N = int.Parse(Console.ReadLine());
+        int div5 = new int();
+        for (int i = 1; i <= N; i++)
+        {
+            for (int j = 5; j <= N; j=j*5)
+            {
+                if (i % j == 0)
+                {
+                    div5 += 1;
+                }
+            }
+        }
+        Console.WriteLine(div5);
     }
 }
