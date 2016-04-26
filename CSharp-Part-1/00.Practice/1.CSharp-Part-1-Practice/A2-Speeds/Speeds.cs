c50d1f3cf5ded1a230e24c4a293ebc163e51eede
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2016-04-26 22:47:27 +0300

    Exam CSharp

---
+++ CSharp-Part-1/00.Practice/1.CSharp-Part-1-Practice/A2-Speeds/Speeds.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Numerics;
+
+class Speeds
+{
+    static void Main()
+    {
+        int c = int.Parse(Console.ReadLine());
+        int count = 1;
+        decimal frontSpeed = decimal.Parse(Console.ReadLine());
+        decimal speedSum = frontSpeed;
+        int finalCount = 1;
+        decimal finalSpeedSum = speedSum;
+        decimal nextSpeed = 0;
+        for (int i = 0; i < c - 1; i++)
+        {
+            decimal speed = decimal.Parse(Console.ReadLine());
+            nextSpeed = speed;
+            if (nextSpeed > frontSpeed)
+            {
+                speedSum += nextSpeed;
+                count++;
+                if (count > finalCount)
+                {
+                    finalCount = count;
+                    finalSpeedSum = speedSum;
+                }
+                else if (count == finalCount)
+                {
+                    if (speedSum > finalSpeedSum)
+                    {
+                        finalSpeedSum = speedSum;
+                    }
+                }
+            }
+            if (nextSpeed <= frontSpeed)
+            {
+                count = 1;
+                if (count > finalCount)
+                {
+                    finalSpeedSum = speedSum;
+                }
+                else if (count == finalCount)
+                {
+                    if (speedSum > finalSpeedSum)
+                    {
+                        finalSpeedSum = speedSum;
+                    }
+                }
+                speedSum = nextSpeed;
+            }
+            frontSpeed = nextSpeed;
+        }
+        Console.WriteLine(finalSpeedSum);
+    }
+}
---
+++ CSharp-Part-1/00.Practice/1.CSharp-Part-1-Practice/A5-Conductor/Conductor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Conductor
+{
+    static void Main()
+    {
+        int p = int.Parse(Console.ReadLine());
+        int m = int.Parse(Console.ReadLine());
+        int ticket = 0;
+        int test = 0;
+        int testTicket = 0;
+        int dig = Convert.ToString(p, 2).Length;
+        for (int i = 0; i < m; i++)
+        {
+            ticket = int.Parse(Console.ReadLine());
+            for (int j = 0; j < 32-dig; j++)
+            {
+                testTicket = ticket >> j;
+                for (int k = 0; k < dig; k++)
+                {
+                    int mask = (p >> k) & 1;
+                    if (((testTicket >> k) & 1) == mask) { test++; }
+                }
+                if (test == dig) 
+                {
+                    for (int pp = j; pp < j+dig; pp++)
+                    {
+                        int zero = ~(1 << pp);
+                        ticket = (int)(ticket & zero);
+                    }
+                }
+                test = 0;
+            }
+            Console.WriteLine(ticket);
+        }
+    }
+}
