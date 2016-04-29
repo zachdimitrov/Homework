eae586dc649e89c2ab31d1b821774ca4f109f5ff
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2016-04-29 23:13:38 +0300

    C Sharp Part 1 Exam - Solutions

---
+++ CSharp-Part-1/CSharp-1-Exam-26.4.2016/Task-1-Squirrels/Squirrels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Squirrels
+{
+    static void Main()
+    {
+        double t = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double s = double.Parse(Console.ReadLine());
+        double n = double.Parse(Console.ReadLine());
+        double result = 0;
+
+        double tails = t * b * s * n;
+        if (tails % 2 == 0)
+        {
+            result = (double)tails * 376439;
+        }
+        else
+        {
+            result = ((double)tails / 7);
+        }
+        Console.WriteLine("{0:F3}", result);
+    }
+}
