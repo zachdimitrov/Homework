dc5ed68d5d9f62524389771285175a115e0d67f8
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2016-05-05 10:24:07 +0300

    changes on CSS

---
+++ CSharp-Part-1/00.Practice/Chef-1-PieceOfCake/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public class CharsInString
+{
+    public static void Main()
+    {
+
+        int count = 0;
+        bool rev = false;
+        int tests = int.Parse(Console.ReadLine());
+        for (int i = 0; i < tests; i++)
+        {
+            string s = Console.ReadLine();
+            if (s.Length % 2 == 0)
+            {
+                for (int j = 0; j < s.Length; j++)
+                {
+                    for (int k = 0; k < s.Length; k++)
+                    {
+                        if (j != k)
+                        {
+                            if (s[j] == s[k])
+                            {
+                                count++;
+                            }
+                        }
+                    }
+                    if (count == (s.Length / 2) - 1)
+                    {
+                        rev = true;
+                        break;
+                    }
+                    count = 0;
+                }
+            }
+            if (rev)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+            rev = false;
+            count = 0;
+        }
+    }
+}
---
+++ CSharp-Part-1/00.Practice/Chef-2-LifeUniverseEverything/LifeUniverseEverything.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class LifeUniverseEverything
+{
+    static void Main()
+    {
+        while (true)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (n != 42)
+            {
+                Console.WriteLine(n);
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
