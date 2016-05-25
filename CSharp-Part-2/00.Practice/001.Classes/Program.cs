a9205da15b7145e78a7ae45202e62113e8dd616d
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2016-05-25 09:45:06 +0300

    some changes

---
+++ CSharp-Part-2/00.Practice/001.Classes/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace TestClasses
+{
+    class Person
+    {
+        public string Name { get; private set; }
+        private int age;
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    age = value;
+                }
+            }
+        }
+        public Person()
+        {
+            Name = "Unnamed";
+            Age = 0;
+        }
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public void AssignFakeName(string newName)
+        {
+            Name = newName;
+        }
+    }
+    class ProgramToTestClasses
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("Pesho", 0);
+            p1.Age++;
+            p1.AssignFakeName("Gosho");
+            Console.WriteLine(p1.Name + " " + p1.Age);
+        }
+    }
+}
--- CSharp-Part-2/05.ClassesAndObjects/07.Workdays/Workdays.cs
+++ CSharp-Part-2/05.ClassesAndObjects/07.Workdays/Workdays.cs
@@ -1,15 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _07.Workdays
+class Workdays
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+         
     }
 }
