9a58086fcb054560de3878f31c7809e648620254
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2016-06-12 23:09:43 +0300

    OOP Practice

---
+++ CSharp-OOP/00.CSharp-OOP-Practice/PR-1-CatSystem/Cat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR_1_CatSystem
+{
+    public class Cat
+    {
+        public static int NumberOfLegs
+        {
+            get
+            {
+                return 4;
+            }
+        }
+        public Cat(CatColor color)
+        {
+            this.Color = color;
+        }
+        public string Name { get; set; }
+        public int Age { get; private set; }
+        public Owner Owner { get; set; }
+        public CatColor Color { get; private set; }
+        public static string CatSais()
+        {
+            return "Mew!";
+        }
+    }
+}
