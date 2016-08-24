f1f3b3f609c103a293ee45868a584b0d2a401b5c
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2016-08-25 01:24:11 +0300

    High Quality Code HW started

---
+++ CSharp-HQC-1/02.CodeFormatting/CodeFormatting/IWriter.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="IWriter.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace CodeFormatting
+{
+    public interface IWriter
+    {
+        void Write(string message);
+
+        void WriteLine(string message);
+    }
+}
