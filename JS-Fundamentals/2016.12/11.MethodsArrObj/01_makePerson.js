49ee013beb3f039c5cd763fb13ae1972fae5a4f7
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-01-04 10:21:02 +0200

    function object methods HW

---
+++ JS-Fundamentals/2016.12/11.MethodsArrObj/01_makePerson.js
@@ -0,0 +1,51 @@
+function makePerson(fname, lname, age = 1, gender = 'f') {
+
+    function getGender(string) {
+        g = string.toLowerCase();
+
+        if (g === 'f' || g === 'female') {
+            return true;
+        }
+        else if (g === 'm' || g === 'male') {
+            return false;
+        }
+    }
+
+    let person = {
+        firstname: fname,
+        lastname: lname,
+        age: age,
+        gender: getGender(gender),
+        tostring: function () {
+            let gen = 'male';
+            if (this.gender) {
+                gen = 'female';
+            }
+            return `${this.firstname} ${this.lastname}, ${this.age}, ${gen}`;
+        }
+    };
+    return person;
+}
+
+let info = [
+    ['Gosho', 'Petkov', 27, 'm'],
+    ['Penka', 'Ivanova', 66, 'f'],
+    ['Stoiko', 'Ganev', 10, 'm'],
+    ['John', 'Smith', 28, 'male'],
+    ['Tomas', 'Anderson', 21, 'm'],
+    ['Pete', 'Sampras', 42, 'm'],
+    ['Grigor', 'Dimitrov', 25, 'male'],
+    ['Cecka', 'Cacheva', 55, 'm'],
+    ['Stanka', 'Zlateva', 25, 'female'],
+    ['Ivanka', 'Trump', 45, 'f'],
+];
+
+let people = [];
+
+for (let i of info) {
+    let p = makePerson(i[0], i[1], i[2], i[3]);
+    people.push(p);
+}
+
+console.log(people);
+people.forEach(p => console.log(p.tostring()));
